92033792fd9c20d77424b6388b9726392934332a
Sam Szreter <s.szreter@gmail.com>
2017-01-30 19:11:29 +0000

    Added isNew function to study ctrl

--- static/scripts/controllers/study.js
+++ static/scripts/controllers/study.js
@@ -1,6 +1,25 @@
 'use strict';
 
 angular.module('yondeokuApp')
-.controller('studyCtrl', function($scope) {
+.controller('studyCtrl', function($scope, $http, ServerService, DataService, LemmaService, $timeout) {
+
+	$scope.ServerService = ServerService;
+
+	var isNew = function (lemma) {
+		var knownList = DataService.userdata.known;
+		var readDict = LemmaService.getReadLemmas(DataService.userdata);
+		var readCount = readDict[lemma]
+		if (knownList.indexOf(lemma) >= 0) {
+			return false;
+		} else if (readCount === undefined) {
+			return true;
+		} else if (readCount >= DataService.userdata.threshold) {
+			return false;
+		} else {
+			return true;
+		}
+	};
+
+//	$timeout(isNew, 100);
 
 });
--- static/scripts/services/lemma.js
+++ static/scripts/services/lemma.js
@@ -4,30 +4,23 @@ angular.module('yondeokuApp')
 .factory('LemmaService', function($http) {
 
 	var LemmaService = {
-		getReadLemmas: function(Blocks) {
+		getReadLemmas: function (User) {
 			var result = {};
-			var inspect = {};
-			for (var i in Blocks) {
-				var Block = Blocks[i];
+			for (var i in User.Blocks) {
+				var Block = User.Blocks[i];
 				for (var j in Block.lemmaList) {
 					var lemmaList = Block.lemmaList[j];
-					console.log(lemmaList);
-					console.log(typeof(lemmaList));
-					var token = Block.tokens[j].tokenText;
 					for (var k in lemmaList) {
 						var lemma = lemmaList[k];
 						if (!(lemma in result)) {
 							result[lemma] = 1;
-							inspect[lemma] = [token];
 						} else {
 							result[lemma] = result[lemma] + 1;
-							inspect[lemma].push(token);
 						}
 					}
 				}
 			}
 			return result;
-//			return inspect;
 		}
 	};
 
